233a8a7e6d2561a57d324cb22aa3e8884d09c97c
adhishrestha <aditya16stha@gmail.com>
2026-05-20 18:10:46 +0545

    feat(routing): setup application routes and layout structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
