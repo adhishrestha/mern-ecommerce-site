2efc0bfd519c333ea09a74034785de4bb66876ee
adhishrestha <aditya16stha@gmail.com>
2026-05-23 18:51:48 +0545

    style(container): update max-width and layout-system

--- frontend/src/components/ui/Container.jsx
+++ frontend/src/components/ui/Container.jsx
@@ -3,7 +3,7 @@ import React, { Children } from 'react';
 const Container = ({ children, className = '' }) => {
   return (
     <div
-      className={`w-full max-w-[1440px] mx-auto px-4 sm:px-8 md:px-12 lg:px-20 xl:px-32 2xl:px-41.25 ${className}`}
+      className={`mx-auto w-full max-w-360 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-32 2xl:px-41.25 ${className}`}
     >
       {children}
     </div>
